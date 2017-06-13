6471638b3ba2d2d7c22bfb225813f7f357820c61
kittysquee <kittysquee@gmail.com>
2017-06-13 17:06:54 +0100

    Adds stripe payment

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   resources :tasks
+  resources :charges
+
+  root to: "tasks#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
